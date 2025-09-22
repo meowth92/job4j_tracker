247460d6151f00cbf26592417184f4e125360249
marat92 <sodnopov@list.ru>
2025-09-22 22:38:37 +0700

    1.7. Конструктор [#173889 #34968]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error errorNew = new Error(true, 12, "message");
+        error.printInfo();
+        errorNew.printInfo();
+
+    }
+
+    public void printInfo() {
+        System.out.println("Активность: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+}
