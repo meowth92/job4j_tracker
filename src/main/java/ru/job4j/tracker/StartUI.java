aa5886cf176349056c835c5de141c37d259350fe
marat92 <sodnopov@list.ru>
2025-09-24 13:29:47 +0700

    3.6. Реализация класса StartUI. Вывод заявок по имени [#500749 #183304]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -39,7 +39,14 @@ public class StartUI {
                 } else {
                     System.out.println("Ошибка замены заявки.");
                 }
-        }  else if (select == 4) {
+            }  else if (select == 3) {
+            System.out.println("=== Удаление заявки ===");
+            System.out.print("Введите id: ");
+            int id = Integer.parseInt(scanner.nextLine());
+            Item item = tracker.findById(id);
+            tracker.delete(id);
+            System.out.println(item != null ? "Заявка удалена успешно." : "Ошибка удаления заявки.");
+            } else if (select == 4) {
                 System.out.println("=== Вывод заявки по id ===");
                 System.out.print("Введите id: ");
                 int id = Integer.parseInt(scanner.nextLine());
@@ -49,13 +56,18 @@ public class StartUI {
                 } else {
                     System.out.println("Заявка с введенным id: " + id + " не найдена.");
                 }
-        } else if (select == 3) {
-            System.out.println("=== Удаление заявки ===");
-            System.out.print("Введите id: ");
-            int id = Integer.parseInt(scanner.nextLine());
-            Item item = tracker.findById(id);
-            tracker.delete(id);
-            System.out.println(item != null ? "Заявка удалена успешно." : "Ошибка удаления заявки.");
+            } else if (select == 5) {
+                System.out.println("=== Вывод заявок по имени ===");
+                System.out.print("Введите имя: ");
+                String name = scanner.nextLine();
+                Item[] items = tracker.findByName(name);
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Заявки с именем: " + name + " не найдены.");
+                }
             } else if (select == 6) {
                 run = false;
             }
